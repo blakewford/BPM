c6bc4548f622bc96b9dbe21cb38a2336c2a4baad
Blake Ford <livewire@Macbook.local>
2013-06-14 17:46:51 -0500

    share ruby gen source

--- platformresources8080.rb
+++ platformresources8080.rb
@@ -1,21 +1,4 @@
+require "sharedparser.rb"
+
 filename_root = $0[0, $0.length-3]
-filename = "./jni/"+filename_root+".h"
-file = File.new(filename, "r")
-line = file.gets
-while (line !=nil && line != "/*Start Opcodes*/\n")
-    line = file.gets
-end
-puts "/** Automatically generated file. DO NOT MODIFY */\npackage org.starlo.bytepusher;\npublic enum "+filename_root.capitalize()+" {"
-line = file.gets
-while (line !=nil && line != "/*End Opcodes*/\n")
-    nextline = file.gets
-    if(nextline != "/*End Opcodes*/\n")
-	line = line[0, line.length-2]+","
-	puts line.upcase
-    else
-	puts line.upcase
-    end
-    line = nextline
-end
-file.close
-puts "}"
+parser(filename_root)
